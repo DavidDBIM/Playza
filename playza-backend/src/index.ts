b952812644d770c956a70e1e7e598e63bbd0b2f5
Guseltony <devguselt@gmail.com>
2026-03-25 00:09:30 +0100

    fixing cors error

--- playza-backend/src/index.ts
+++ playza-backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(helmet())
-app.use(cors({ origin: process.env.FRONTEND_URL || '*' }))
+app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' || '*' }))
 app.use(express.json())
 
 app.get('/health', (_, res) => {
