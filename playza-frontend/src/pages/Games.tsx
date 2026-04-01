ca9306d8afd7afb0de5d126aacec3ab7b740abb2
muizcal <125582144+muizcal@users.noreply.github.com>
2026-04-01 06:34:45 +0100

    Add Games component to render FullGame

---
+++ playza-frontend/src/pages/Games.tsx
@@ -0,0 +1,11 @@
+import FullGame from "@/components/game/FullGame";
+
+const Games = () => {
+  return (
+    <div className="flex-1 min-w-0 overflow-hidden">
+      <FullGame />
+    </div>
+  );
+};
+
+export default Games;
