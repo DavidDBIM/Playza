eba657ad8f7c12c80f920f2b4bb3d183772b3646
DavidDBIM <97288668+DavidDBIM@users.noreply.github.com>
2026-06-22 07:20:37 +0100

    Update Sidebar.tsx

--- playza-admin/src/components/layout/Sidebar.tsx
+++ playza-admin/src/components/layout/Sidebar.tsx
@@ -46,6 +46,7 @@ const NAV_ITEMS = [
   { path: '/ambassadors', pathMatch: '/ambassadors', icon: MdEmojiPeople, label: 'Ambassadors' },
   { path: '/social-tasks', pathMatch: '/social-tasks', icon: MdShare, label: 'Social Tasks' },
   { path: '/quiz-tournaments', pathMatch: '/quiz-tournaments', icon: MdQuiz, label: 'Quiz Tournaments' },
+  { path: '/chess-tournaments', pathMatch: '/chess-tournaments', icon: MdSportsEsports, label: 'Chess Tournaments' },
   { path: '/sponsors', pathMatch: '/sponsors', icon: MdHandshake, label: 'Sponsors' },
   { path: '/referral-payouts', pathMatch: '/referral-payouts', icon: MdCurrencyExchange, label: 'Referral Payouts' },
   { path: '/notifications', pathMatch: '/notifications', icon: MdNotifications, label: 'Notifications' },
